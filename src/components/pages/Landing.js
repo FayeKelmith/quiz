934ca0a1e4ae59703669809fe2e305741328a846
Faye Kelmith Rernyuy <fayekelmith@gmail.com>
2023-09-12 16:54:05 +0530

    started adding elements

---
+++ src/components/pages/Landing.js
@@ -0,0 +1,12 @@
+function Landing() {
+  return (
+    <div className="bg-gradient-to-bl from-cyan-800 to-slate-900">
+      <nav>
+        <h3 className="text-left text-5xl text-white ">Titan</h3>
+        <h3 className="text-right">My quiz App</h3>
+      </nav>
+    </div>
+  );
+}
+
+export default Landing;
