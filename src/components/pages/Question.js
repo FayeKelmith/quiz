101c06a2c72da1e57246253f5fa782fbb611382b
Faye Kelmith Rernyuy <fayekelmith@gmail.com>
2023-09-15 00:37:53 +0530

    one last step

--- src/components/pages/Question.js
+++ src/components/pages/Question.js
@@ -4,7 +4,7 @@ import { Truebtn, Falsebtn, Closebtn } from "../atoms/Responsebtn";
 
 const Question = () => {
   //TODO: to use choice here in receiving users' in put.
-  // const [choice, setChoice] = useState(Array(10).fill(null));
+
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -40,23 +40,40 @@ const Question = () => {
 
 export const QuestionBox = ({ data }) => {
   const [num, setNum] = useState(0);
+  const [choice, setChoice] = useState(Array(10).fill(null));
+
+  const handleClick = (val) => {
+    const nextChoice = choice.slice();
+    if (num < 10) {
+      nextChoice[num] = val;
+      setChoice(nextChoice);
+      setNum(num + 1);
+    }
+  };
 
   const display = () => {
     if (num < 10) {
-      return data.length ? data[num].question : null;
+      console.log(num, choice);
+      return data.length ? data[num].question.replace(".", "?") : null;
     }
   };
   return (
     <div className="min-w-3/5  text-center mx-auto min-h-5/6 ">
-      <div className="w-80 pt-5 px-10 mx-auto my-0 ">{display()}</div>
+      <div className="w-80 h-40 bg-slate-200 pt-5 px-10 mx-auto my-0 ">
+        {display()}
+      </div>
       <h3 className="text-3xl bg-night text-day my-5 mx-auto rounded-md w-fit px-10 py-3">
         What do you think?
       </h3>
       <div className="my-0 mx-auto flex place-content-center mb-0">
-        <span className="mx-16" attr="Truebtn" onClick={() => setNum(num + 1)}>
+        <span value={true} className="mx-16" onClick={() => handleClick(true)}>
           <Truebtn />
         </span>
-        <span className=" mx-16" attr="False" onClick={() => setNum(num + 1)}>
+        <span
+          className=" mx-16"
+          attr="False"
+          onClick={() => handleClick(false)}
+        >
           <Falsebtn />
         </span>
       </div>
@@ -64,3 +81,9 @@ export const QuestionBox = ({ data }) => {
   );
 };
 export default Question;
+
+//TODO: 1. Structure my code and start using context API
+//solve problem of not getting the last results
+//build the results page
+//build guidelines page with it's agreement button making use of <Outlet />
+//learn dom routing and use it to link these pages
