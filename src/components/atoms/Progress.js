f2c57fc5b284ec582bac2b1e879e42df66b6ca85
Faye Kelmith Rernyuy <fayekelmith@gmail.com>
2023-09-16 14:16:59 +0530

    progress done

--- src/components/atoms/Progress.js
+++ src/components/atoms/Progress.js
@@ -1,5 +1,16 @@
-import { Progress } from "@material-tailwind/react";
+const Progress = ({ percentage }) => {
+  return (
+    <div className="h-2 rounded-t-md w-full bg-slate-400">
+      <div
+        style={{ width: `${percentage * 10}%` }}
+        className={`h-full ${
+          percentage >= 5
+            ? "bg-gradient-to-r from-primary to-lining"
+            : "bg-gradient-to-r from-lining to-primary"
+        }`}
+      ></div>
+    </div>
+  );
+};
 
-export function ProgressVariant() {
-  return <Progress value={50} />;
-}
+export default Progress;
--- src/components/pages/Question.js
+++ src/components/pages/Question.js
@@ -3,6 +3,7 @@ import { Truebtn, Falsebtn, Closebtn } from "../atoms/Responsebtn";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Progress from "../atoms/Progress";
 
 const Question = () => {
   const [num, setNum] = useState(0);
@@ -53,13 +54,12 @@ const Question = () => {
       {num < 10 ? (
         <div className="container">
           <div className="bg-slate-100 text-center shadow-lg my-[20vh] mx-auto w-3/5 min-h-[60vh]">
+            <div className="">
+              <Progress percentage={num} />
+            </div>
             <div className="flex">
               {/*TODO: Timer as atom  */}
               <div className="flex-auto">Timer</div>
-              <div className="flex-auto">
-                {/*TODO:   Score card as atom*/}
-                Score Card
-              </div>
               <Closebtn className="flex-none " />
             </div>
             <div className="min-w-3/5  text-center mx-auto min-h-5/6 ">
