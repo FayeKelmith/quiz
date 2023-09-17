cb11cb5da6edd5d6b1bee85ed7e339f1748cd1be
Faye Kelmith Rernyuy <fayekelmith@gmail.com>
2023-09-17 15:37:17 +0530

    about to provide choice during fetch

--- src/components/organisms/CardDisplay.js
+++ src/components/organisms/CardDisplay.js
@@ -3,21 +3,14 @@ import { PiCarSimpleBold } from "react-icons/pi";
 import { MdSportsMartialArts } from "react-icons/md";
 import { Card } from "../atoms/Cards";
 const CardDisplay = () => {
+  const space = " text-7xl mx-auto my-0 flex";
   return (
-    <div className=" grid grid-cols-3 gap-x-10 w-fit my-0 mx-auto mt-20">
-      <Card
-        category="Animals"
-        icon={<FaShieldDog className=" text-7xl mx-auto my-0 mt-5 flex" />}
-      />
-      <Card
-        category="Vehicles"
-        icon={<PiCarSimpleBold className=" text-7xl mx-auto my-0 mt-5 flex" />}
-      />
+    <div className="grid grid-cols-3 gap-x-10 w-fit my-0 mx-auto mt-10">
+      <Card category="Animals" icon={<FaShieldDog className={space} />} />
+      <Card category="Vehicles" icon={<PiCarSimpleBold className={space} />} />
       <Card
         category="Athletics"
-        icon={
-          <MdSportsMartialArts className=" text-7xl mx-auto my-0 mt-5 flex" />
-        }
+        icon={<MdSportsMartialArts className={space} />}
       />
     </div>
   );
--- src/components/pages/Guide.js
+++ src/components/pages/Guide.js
@@ -1,7 +1,11 @@
+import CardDisplay from "../organisms/CardDisplay";
 const Guide = () => {
   return (
     <div>
-      <h1 className="text-5xl">THis is the guide page!</h1>
+      <div className="container border drop-shadow-md w-[60vw] h-[60vh] my-[20vh] mx-auto text-center">
+        <h1 className="text-5xl my-4 ">Pick a challenge!</h1>
+        <CardDisplay />
+      </div>
     </div>
   );
 };
