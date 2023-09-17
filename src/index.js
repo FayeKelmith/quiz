5d65140c574bb73808e14b1ac568be2f7ac2a662
Faye Kelmith Rernyuy <fayekelmith@gmail.com>
2023-09-17 14:00:58 +0530

    branched to work on routing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import Error from "./components/pages/Error";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 //import Guide from "./components/pages/Guide";
 import Landing from "./components/pages/Landing";
+import Guide from "./components/pages/Guide";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
-    errorElement: <Error />,
-    // children: {
-    //   path: "./Guide",
-    //   element: <Guide />,
-    // },
+    //errorElement: <Error />,
+  },
+  {
+    path: "/guide",
+    element: <Guide />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
